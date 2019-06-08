7e7b2737c2f3d447b8daaa411475c01d0a835eeb
Panossa <panoshd.kp@gmail.com>
2019-06-08 13:52:10 +0200

    Fixed start.bat

--- start.bat
+++ start.bat
@@ -9,6 +9,8 @@ copy /y *.sav "../SaveGames"
 set start=%date:~0,2%-%date:~3,2%-%date:~8,2%@%time:~0,5%
 ::Start Satis, wait for end
 start "" com.epicgames.launcher://apps/CrabEA?action=launch
+echo Waiting for the game to start (10 seconds)
+TIMEOUT /T 10 /Nobreak >nul
 echo Waiting for game to close...
 
 :LOOP
@@ -31,6 +33,7 @@ copy /y *.sav "../satissaves"
 cd "../satissaves"
 ::Push data
 git add *.sav
+git add *.bat
 git commit -m "%msg%"
 git push
 echo Save game uploaded!
